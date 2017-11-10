82605d77ab9e4233540b43d9193e93dba7f7e638
Rob Parr <rob@kingslandlinassi.com>
2017-11-10 09:40:04 +0000

    refactor index.php to increase readability and add try catch to catch empty post to add-todo

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
 require 'core/bootstrap.php';
+$router = Router::load('routes.php');
 
-require Router::load('routes.php')
-	->direct(Request::uri(), Request::method());
+try {
+    require $router->direct(Request::uri(), Request::method());
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
+
--- routes.php
+++ routes.php
@@ -3,5 +3,3 @@
 $router->get('', 'controllers/index.php');
 $router->get('about', 'controllers/about.php');
 $router->post('add-todo', 'controllers/add-todo.php');
-
-var_dump($router);
